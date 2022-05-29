c82782d96a2617bb3f64aa704bc05ba8f5aab3a3
jlcodes <jlcoding7@gmail.com>
2022-05-29 18:20:37 -0500

    added endRound func

--- javascript/monster_killer_game/assets/scripts/app.js
+++ javascript/monster_killer_game/assets/scripts/app.js
@@ -6,13 +6,23 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
 
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        alert('You used your bonus life!');
+        setPlayerHealth(initialPlayerHealth);
+    }
+    
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You won!');
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
