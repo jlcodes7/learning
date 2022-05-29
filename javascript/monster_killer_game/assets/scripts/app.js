2eaea463a4b79441f5e5e0d1206b25e927ab375c
jlcodes <jlcoding7@gmail.com>
2022-05-29 16:45:16 -0500

    created strongAttackHandler function

--- javascript/monster_killer_game/assets/scripts/app.js
+++ javascript/monster_killer_game/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const PLAYER_ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -23,5 +24,21 @@ function attackHandler() {
     }
 }
 
-attackBtn.addEventListener('click', attackHandler);
+function strongAttackHandler() {
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
 
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert('You have a draw!');
+    }
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
