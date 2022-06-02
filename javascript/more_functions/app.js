5dff8d468caec6af1d9a31cdd7fd80b6c5851df3
jlcodes <jlcoding7@gmail.com>
2022-06-02 12:33:08 -0500

    created getPChoice, getCChoice, and getWinner func

--- javascript/more_functions/app.js
+++ javascript/more_functions/app.js
@@ -1,7 +1,52 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-    console.log('Game is starting...');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER WINS';
+
+let gameIsRunning = false;
+
+getPlayerChoice = function(){
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice! We chose ${ROCK} for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
+
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
+
+const getWinner = function(compChoice, playChoice) {
+    if (compChoice === playChoice) {
+        return RESULT_DRAW;
+    } else if (
+        compChoice === ROCK && playChoice === PAPER || 
+        compChoice === PAPER && playChoice === SCISSORS ||
+        compChoice === SCISSORS && playChoice === ROCK
+    ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
 }
 
 // const person = {
@@ -15,4 +60,13 @@ function startGame() {
 
 // console.dir(startGame);
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', function () {
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    console.log(playerSelection);
+});
