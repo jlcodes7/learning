36613e2a8b567c4a7ca727d530282888d19b2e62
jlcodes <jlcoding7@gmail.com>
2022-06-02 11:14:39 -0500

    created startGame func

--- javascript/more_functions/app.js
+++ javascript/more_functions/app.js
@@ -1,1 +1,18 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('Game is starting...');
+}
+
+// const person = {
+//     name: 'Max',
+//     greet: function greet() {
+//         console.log('Hello there!');
+//     }
+// };
+
+// person.greet();
+
+// console.dir(startGame);
+
+startGameBtn.addEventListener('click', startGame);
