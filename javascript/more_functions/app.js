c9c0b949229179e7d2d1ab209ecc7d8a5abf4ac4
jlcodes <jlcoding7@gmail.com>
2022-06-03 10:41:16 -0500

    updated gameBtn event listner

--- javascript/more_functions/app.js
+++ javascript/more_functions/app.js
@@ -10,44 +10,49 @@ const RESULT_COMPUTER_WINS = 'COMPUTER WINS';
 
 let gameIsRunning = false;
 
-getPlayerChoice = function(){
-    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
-    if (
-        selection !== ROCK &&
-        selection !== PAPER &&
-        selection !== SCISSORS
-    ) {
-        alert(`Invalid choice! We chose ${ROCK} for you!`);
-        return DEFAULT_USER_CHOICE;
-    }
-    return selection;
+getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${ROCK} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 };
 
-const getComputerChoice = function () {
-    const randomValue = Math.random();
-    if (randomValue < 0.34) {
-        return ROCK;
-    } else if (randomValue < 0.67) {
-        return PAPER;
-    } else {
-        return SCISSORS;
-    }
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
 };
 
+const getWinner = (compChoice, playChoice) => 
+compChoice === playChoice
+    ? RESULT_DRAW
+    : (compChoice === ROCK && playChoice === PAPER) ||
+      (compChoice === PAPER && playChoice === SCISSORS) ||
+      (compChoice === SCISSORS && playChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
 
-const getWinner = function(compChoice, playChoice) {
-    if (compChoice === playChoice) {
-        return RESULT_DRAW;
-    } else if (
-        compChoice === ROCK && playChoice === PAPER || 
-        compChoice === PAPER && playChoice === SCISSORS ||
-        compChoice === SCISSORS && playChoice === ROCK
-    ) {
-        return RESULT_PLAYER_WINS;
-    } else {
-        return RESULT_COMPUTER_WINS;
-    }
-}
+    // if (compChoice === playChoice) {
+    //     return RESULT_DRAW;
+    // } else if (
+    //     compChoice === ROCK && playChoice === PAPER ||
+    //     compChoice === PAPER && playChoice === SCISSORS ||
+    //     compChoice === SCISSORS && playChoice === ROCK
+    // ) {
+    //     return RESULT_PLAYER_WINS;
+    // } else {
+    //     return RESULT_COMPUTER_WINS;
+    // }
 
 // const person = {
 //     name: 'Max',
@@ -60,14 +65,22 @@ const getWinner = function(compChoice, playChoice) {
 
 // console.dir(startGame);
 
-startGameBtn.addEventListener('click', function () {
-    if (gameIsRunning) {
-        return;
-    }
-    gameIsRunning = true;
-    console.log('Game is starting...');
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-    const winner = getWinner(computerChoice, playerChoice);
-    console.log(winner);
+startGameBtn.addEventListener('click', () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  let message = `You picked ${playerChoice}, the computer picked ${computerChoice}, therefore you`
+  if (winner === RESULT_DRAW) {
+      message = message + 'had a draw.';
+  } else if (winner === RESULT_PLAYER_WINS) {
+      message = message + 'won!'
+  } else {
+      message = message + 'lost.'
+  }
+  console.log(winner);
 });
