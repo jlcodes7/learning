ae38850ee9e55700e1219beaafcbff9f993df43e
jlcodes <jlcoding7@gmail.com>
2022-06-04 13:20:58 -0500

    completed lectures

--- javascript/working_with_the_dom/app.js
+++ javascript/working_with_the_dom/app.js
@@ -1,3 +1,23 @@
-// what is the DOM?
-// DOM = document object model
+// // what is the DOM?
+// // DOM = document object model
 
+// const h1 = document.getElementById('main-title');
+// h1.textContent = 'Some new Title!';
+// h1.style.color = 'white';
+// h1.style.backgroundColor = 'black';
+
+// const li = document.querySelector('li:last-of-type');
+// li.textContent = li.textContent + ' (Changed!)';
+
+// const body = document.body;
+
+// const listItemElements = document.getElementsByTagName('li');
+
+// for (const listItemElem of listItemElements) {
+//     console.dir(listItemElem);
+// }
+
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+
+console.log(firstLi);
